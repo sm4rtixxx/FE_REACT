a689b686f9178ca773bc1bc99cf79d690e48edca
docenteFulvia <docentefulvia@proton.me>
2026-06-11 18:01:06 +0200

    visualizzazione utenti (commentato tutto il codice precedente in app.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import MeteoInfo from './components/MeteoInfo.jsx';
 import MeteoRaffa from './components/MeteoRaffa.jsx';
 import Card from './components/Card/Card.jsx';
 import Card2 from './components/Card2/Card2.jsx';
+import Utente from './components/Utente/Utente.jsx';
 
 function App() {
   return (
     <>
-      <h1>PRIMO REACT</h1>
-      <Biglietto nome="Giacomo" destinazione="Roma" />
+      <Utente />
+      {/* <Biglietto nome="Giacomo" destinazione="Roma" />
       <Biglietto nome="Emma" destinazione="Bari" />
       <Biglietto nome="Marta" destinazione="Como" />
       <Biglietto destinazione="Marrachesh" />
       <MeteoInfo />
       <Card />
-      <Card2 />
+      <Card2 /> */}
     </>
   );
 }
